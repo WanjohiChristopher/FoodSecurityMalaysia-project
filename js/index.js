c283c626d23ab27636a824697b9cf891a2f27d98
ELIJAHKUNGU <elijahkungu100@gmail.com>
2021-08-02 19:29:12 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+    // main-banner owl carousel
+$(".banner .owl-carousel").owlCarousel({
+    nav: false,
+    dots: false,
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+    },
+});
+});
+
